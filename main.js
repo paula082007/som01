fe87bb9e548e80377315037a5904b3159b092364
20 <105080646+paula082007@users.noreply.github.com>
2023-10-27 11:05:12 -0300

    ok

---
+++ main.js
@@ -0,0 +1,17 @@
+//Declaração/criação da função TocaSom
+function TocaSomPom(idElementosAudio){
+    document.querySelector(idElementosAudio). play();
+}
+//chamda ou invocação da função tocaSomPom ao clicar no botão
+const listadeteclas = document.querySelectorAll('.tecla')]
+let contador = 0;
+
+white(contador < listadeteclas.length){
+    const efeito = listadeteclas[contador]. classList[1]
+    const iAudio = "#som_" + efeito 
+    litaDeTeclas[0]. onclick = function;(){
+        TocaSomPom(iAudio)
+}
+    contador = contador + 1;
+    console.long(contador);
+}
