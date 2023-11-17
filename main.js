e5b6de4e8e0906208998975d48d01f24b20c5154
20 <105080646+paula082007@users.noreply.github.com>
2023-11-17 11:07:00 -0300

    ok

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ function TocaSomPom(idElementosAudio){
     document.querySelector(idElementosAudio). play();
 }
 //chamda ou invocação da função tocaSomPom ao clicar no botão
-const listadeteclas = document.querySelectorAll('.tecla')]
-let contador = 0;
-
-white(contador < listadeteclas.length){
-    const efeito = listadeteclas[contador]. classList[1]
-    const iAudio = "#som_" + efeito 
-    litaDeTeclas[0]. onclick = function;(){
-        TocaSomPom(iAudio)
+const listadeteclas = document.querySelectorAll('.tecla')
+for(let contador = 0; contador < listadeteclas.length; contador++){
+    const tecla = listadeteclas[contador]
+    const efeito =  teclaclassList[1]
+    const iAudio = `#som_${efeito}`;
+    teclaonclick = function(){
+        TocaSom(iAudio)
 }
-    contador = contador + 1;
-    console.long(contador);
+tecla.onkeydwon =function(evento){
+    if(evento.code!='Tab')
+    tecla.classList.add('ativa')
+}
+    tecla.onkeydwon =function(){  
+        teclaclassList.remove('ativa')
+}
 }
