948b61672aeba7c5e054e92673ed68e317c2b97d
20 <105080646+paula082007@users.noreply.github.com>
2023-11-24 11:17:34 -0300

    ok

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function TocaSomPom(selectorElemento){
+function TocaSom(selectorElemento){
     const elemento = document.querySelector(selectorElemento);
 
     if(elemento && elemento.localName === 'audio'){
-        elemento.play9();
+        elemento.play();
 }
     else {
-        comsole.log('elementom ou selector não encontrado!')
+        comsole.log('elemento ou selector não encontrado!')
     }
 
 }
@@ -14,14 +14,14 @@ const listadeteclas = document.querySelectorAll('.tecla')
 for(let contador = 0; contador < listadeteclas.length; contador++){
 
     const tecla = listadeteclas[contador]
-    const efeito =  teclaclassList[1]
+    const efeito =  tecla.classList[1]
     const iAudio = `#som_${efeito}`;
-    
+
     teclaonclick = function(){
               TocaSom(iAudio)
 }
 tecla.onkeydwon =function(evento) {
-     console.log(evento.cod == `Space`)
+     console.log(evento.cod == 'Space')
 
     if(evento.code =='Tab') {
     tecla.classList.add('ativa')
